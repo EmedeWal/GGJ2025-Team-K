3ecb013105326e24a8194f44c6181f8275150783
1NTROO <95148848+1NTROO@users.noreply.github.com>
2025-01-24 22:55:09 +0100

    bubble interaction

--- Assets/Scripts/CapturedByBubble.cs
+++ Assets/Scripts/CapturedByBubble.cs
@@ -9,6 +9,9 @@ public class CapturedByBubble : MonoBehaviour
         get => _bubble;
         set => _bubble = value;
     }
+
+    [Range(0, 1)]
+    [SerializeField] private float _transparency = 0.7f;
     void Start()
     {
         
@@ -34,9 +37,9 @@ public class CapturedByBubble : MonoBehaviour
     void OnCapture()
     {
         _bubble.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
-        var colour = _bubble.GetComponentInChildren<SpriteRenderer>().color;
-        colour.a = 0.5f;
-        _bubble.GetComponentInChildren<SpriteRenderer>().color = colour;
+        var colour = GetComponentInChildren<SpriteRenderer>().color;
+        colour.a = _transparency;
+        GetComponentInChildren<SpriteRenderer>().color = colour;
         GetComponent<Collider2D>().enabled = false;
         transform.SetParent(_bubble.transform);
         transform.SetPositionAndRotation(_bubble.transform.position, Quaternion.identity);
