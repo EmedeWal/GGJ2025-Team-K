57d8015b3a9ccc19765dedaa5e369094c97f5ac6
1NTROO <95148848+1NTROO@users.noreply.github.com>
2025-01-24 23:56:11 +0100

    player attributes v1

---
+++ Assets/Player/Attributes.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Attributes : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth;
+    private float _health;
+    [SerializeField] private GameObject _playerAirBubble;
+    private SpriteRenderer _playerAirBubbleSprite;
+    void Start()
+    {
+        // _playerAirBubbleSprite = _playerAirBubble.GetComponentInChildren<SpriteRenderer>();
+        _health = _maxHealth;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _health -= damage;
+        _playerAirBubble.transform.localScale = new Vector3(_health / _maxHealth, _health / _maxHealth, 1);
+        if (_health <= 0)
+        {
+            print("lol!");
+        }
+    }
+}
