9d168181e441caf2b62ab9a36fcccbee9e261179
NicoNickname <nicolasperez.esquire@gmail.com>
2025-01-24 23:22:21 +0100

    Quick commit for val

--- Assets/Scripts/Enemies/RedKoopa.cs
+++ Assets/Scripts/Enemies/RedKoopa.cs
@@ -2,26 +2,52 @@ using UnityEngine;
 
 public class RedKoopa : MonoBehaviour, IKillable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    /// <summary>
+    /// Enemy that walks left and right, but will not voluntarily drop off the platform it is on
+    /// Could also be made so it can ignore ledges and just walk left and right but don't know if we'll use that
+    /// </summary>
+
+    private Rigidbody2D _rigibody;
+
+    private float speed;
+    private float direction;
+
+    
+    [SerializeField] private string status = "roaming"; //roaming means walking around, bubble means in a bubble (so doing nothing), stunned means right after being released from a bubble it cannot move for a bit
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
     }
 
     // It can do a lil death animation maybe, like falling down off-the-screen while spinning to keep it simple. In any case, it dies
-    public void die()
+    public void death()
     {
 
     }
 
-    void IKillable.OnSpikeHit()
+    void CheckEdge()
     {
-        die();
+        var origin = _rigibody.position + _rigibody.linearVelocity.normalized;
+        var dir = Vector2.down;
+
+        if (Physics2D.Raycast(origin, dir, 2f, LayerMask.GetMask(" Ground")))
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    void IKillable.onSpikeHit()
+    {
+        death();
     }
 }
--- Assets/Scripts/LevelObjects/Spikes.cs
+++ Assets/Scripts/LevelObjects/Spikes.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
+    /// <summary>
+    /// Object that kills the player and enemies on contact, and pops bubbles
+    /// </summary>
+    /// <param name="collision"></param>
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
